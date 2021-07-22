8bc27e26d7a9e8a3b74f2d40642f5ab468e9f9b5
Emilio Garcia <egarciaf2@gmail.com>
2021-07-22 09:20:50 -0600

    feat: Users
    Seed primer usuario añadido

--- resources/views/empresas/create.blade.php
+++ resources/views/empresas/create.blade.php
@@ -35,7 +35,7 @@
               				<div class="row">
               					<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
               						<div class="form-group">
-              							<label for="txtNombre">Nombre</label>
+              							<label for="txtNombre">Nombre de Empresa</label>
               							<input type="text" class="form-control" id="txtNombre" name="txtNombre">
               						</div>
               					</div>
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -60,6 +60,10 @@
 			<li class="nav-item">
 				<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
 			</li>
+
+			<li class="nav-item d-none d-sm-inline-block">
+		      <a href="inicio" class="nav-link text-white">Bienvenido {{ Auth::user()->name }}</a>
+		    </li>
 		</ul>
 
 		<!-- Right navbar links -->
